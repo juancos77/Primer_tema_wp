9c36bd29cd9ae10490fb6061797e60331f5c2a62
Juan Antonio Coscollola <37595524+juancos77@users.noreply.github.com>
2018-04-29 10:20:47 +0200

    footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+</div><!-- #main-container --> 
+
+	<footer id="main-footer">
+		<h4><?php bloginfo('description'); ?></h4>
+	</footer><!-- #main-footer -->
+	<?php wp_footer(); ?>
+
+</body>
+</html>
